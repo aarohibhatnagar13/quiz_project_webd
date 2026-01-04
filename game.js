c8e8f32794cbf17cf266c474fdfb04797fbf843f
aarohibhatnagar13 <aarohibhatnagar2005@gmail.com>
2026-01-04 19:20:09 +0530

    the questions are coming on the game page

--- game.js
+++ game.js
@@ -1,3 +1,29 @@
+let allQuestions = [];
+let currentQuestionIndex = 0;
+let score = 0;
+
+
+function decodeHTML(html) {
+    const txt = document.createElement('textarea');
+    txt.innerHTML = html;
+    return txt.value;
+}
+
+function showQuestion(){
+    const questionContainer = document.getElementById('question');
+    const questionData = allQuestions[currentQuestionIndex];
+    questionContainer.innerHTML = '';
+    if(questionData === undefined){
+        questionContainer.innerHTML = '<h2>No questions available for this category. Please select a different category.</h2>';
+        return;
+    }
+    const questionElement = document.createElement('div');
+    questionElement.classList.add('question');
+    questionElement.innerHTML=
+    `<h2>${decodeHTML(questionData.question)}</h2>`;
+    questionContainer.appendChild(questionElement);
+}
+
 async function loadQuizData() {
   
     const categoryIDs = localStorage.getItem('category');
@@ -15,12 +41,11 @@ async function loadQuizData() {
         const response = await fetch(apiURL);
         const data = await response.json();
 
+
         
         console.log("Success! Data received:");
-        const questions = data.results;
-        for (let i = 0; i < questions.length; i++) {      
-            console.log(`Q${i + 1}: ${questions[i].question}`);
-        }
+        allQuestions = data.results;
+        showQuestion();
         
         
         
@@ -30,4 +55,5 @@ async function loadQuizData() {
 }
 
 loadQuizData();
+
 
